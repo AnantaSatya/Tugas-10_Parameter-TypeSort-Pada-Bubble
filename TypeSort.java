65a320a6818c6f8e7a5fa00a811f32d907a2c49a
AnantaSatya <34617604+AnantaSatya@users.noreply.github.com>
2023-12-10 13:25:07 +0700

    Create TypeSort.java

---
+++ TypeSort.java
@@ -0,0 +1,53 @@
+public class TypeSort {
+
+  public static void main(String[] args) {
+    int[] arr = { 4, 3, 2, 10, 12, 1, 5, 6 };
+
+    System.out.println("Array sebelum diurutkan:");
+    printArray(arr);
+
+    bubbleSort(arr, arr.length, "ASC"); // Panggil bubbleSort untuk pengurutan ascending
+    System.out.println("\nArray setelah diurutkan secara ascending:");
+    printArray(arr);
+
+    bubbleSort(arr, arr.length, "DESC"); // Panggil bubbleSort untuk pengurutan descending
+    System.out.println("\nArray setelah diurutkan secara descending:");
+    printArray(arr);
+  }
+
+  // An optimized version of Bubble Sort
+  static void bubbleSort(int arr[], int n, String TypeSort) {
+    int i, j, temp;
+    boolean swapped;
+
+    for (i = 0; i < n - 1; i++) {
+      swapped = false;
+
+      for (j = 0; j < n - i - 1; j++) {
+        if (TypeSort == "ASC" && arr[j] > arr[j + 1]) {
+          // Swap arr[j] and arr[j+1]
+          temp = arr[j];
+          arr[j] = arr[j + 1];
+          arr[j + 1] = temp;
+          swapped = true;
+        } else if (TypeSort == "DESC" && arr[j] < arr[j + 1]) {
+          // Swap arr[j] and arr[j+1]
+          temp = arr[j];
+          arr[j] = arr[j + 1];
+          arr[j + 1] = temp;
+          swapped = true;
+        }
+      }
+
+      // If no two elements were
+      // swapped by inner loop, then break if (swapped == false)
+      if (swapped == false) break;
+    }
+  }
+
+  static void printArray(int[] arr) {
+    int n = arr.length;
+    for (int i = 0; i < n; ++i) System.out.print(arr[i] + " ");
+    System.out.println();
+  }
+}
